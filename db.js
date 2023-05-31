a1dcda67c15696fe4a8c3bfa619de53c0075c040
Kirubel25 <kirubel667@gmail.com>
2023-05-31 09:59:42 +0300

    env file configured

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "sql_demo",
-    password: "",
-    port: 5432
-})
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
 
-module.exports = pool
+module.exports = pool;
